bae91a986b5a9d0875ddad9872fbc9f4abf5bc0f
Harsh D <112274179+Deku04-print@users.noreply.github.com>
2025-07-29 23:42:07 +0530

    Solving basic problem 2

--- PassengerCounterApp/pass.js
+++ PassengerCounterApp/pass.js
@@ -10,3 +10,17 @@ let myAge = 35
 
 // 2. Log the myAge variable to the console
 console.log(myAge)
+
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+
+
+let myAge=21
+
+let humanDogRatio=7
+
+let myDogAge=myAge*humanDogRatio
+
+console.log(myDogAge)

35d977f189bf6fe3e4d275e9598e888a010ddeff
Harsh D <112274179+Deku04-print@users.noreply.github.com>
2025-08-25 18:31:29 +0530

    Added the newCard() function

--- BlackJack/blackjack.js
+++ BlackJack/blackjack.js
@@ -12,7 +12,7 @@ let cardsEl = document.getElementById("cards-el")
 
 function startGame(){
      cardsEl.textContent = "Cards : " +firstCard+" "+secondCard 
-     sumEl.textContent = "Sum: " + sum
+     sumEl.textContent = "Sum: " + sum 
 
 if (sum <= 20) {
     message = "Do you want to draw a new card? "
@@ -29,4 +29,11 @@ messageEl.textContent=message
 
 function newCard() {
     console.log("Drawing a new card from the deck!")
+    // 1. Create a card variable, and hard code its value to a number (2-11)
+    let card = 7
+
+    // 2. Add the new card to the sum variable
+    sum=sum+card
+    // 3. Call startGame()
+    startGame()
 }

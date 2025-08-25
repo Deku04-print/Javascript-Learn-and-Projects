317ad019619045029ad88cd5acda4de768f7c5f9
Harsh D <112274179+Deku04-print@users.noreply.github.com>
2025-08-25 18:08:59 +0530

    Created the button and its function

--- BlackJack/blackjack.js
+++ BlackJack/blackjack.js
@@ -3,19 +3,25 @@ let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
-// 1. Declare a variable called message and assign its value to an empty string
 let message = ""
 
-// 2. Reassign the message variable to the string we're logging out
+// let sumEl = document.getElementById("sum-el")
+let sumEl = document.querySelector("#sum-el")
+// 2. Create a startGame() function. Move the conditional
+// below (line 11-20) inside the body of the function.
+let messageEl=document.getElementById("message-el")
+function startGame(){
+     sumEl.textContent = "Sum: " + sum
+
 if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
+    message = "Do you want to draw a new card? "
 } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
+    message = "You've got Blackjack! "
     hasBlackJack = true
 } else {
-    message = "You're out of the game! 😭"
+    message = "You're out of the game! "
     isAlive = false
 }
+messageEl.textContent=message
 
-// 3. Log it out!
-console.log(message)
+}

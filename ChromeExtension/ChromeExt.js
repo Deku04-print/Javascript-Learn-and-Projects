50cec768508ba615eeb3178ea79139f9f5edf89b
Harsh D <112274179+Deku04-print@users.noreply.github.com>
2025-09-12 22:44:42 +0530

    Adding the Unordered list field

--- ChromeExtension/ChromeExt.js
+++ ChromeExtension/ChromeExt.js
@@ -1,14 +1,18 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+// 2. Grab the unordered list and store it in a const variable called ulEl
+let ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-    // Push the value from the inputEl into the myLeads array 
-    inputEl.value
-    // instead of the hard-coded "www.awesomeleads.com" value
-    // Google -> "get value from input field javascript"
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
+
+
+for (let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i])
+}
+
 
 

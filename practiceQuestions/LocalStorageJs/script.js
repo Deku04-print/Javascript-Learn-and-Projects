bf0ad887ad2eaa392f1be9df2394546dd69ec583
Harsh D <112274179+Deku04-print@users.noreply.github.com>
2025-09-19 02:16:58 +0530

    Solved Localstorrage problem

---
+++ practiceQuestions/LocalStorageJs/script.js
@@ -0,0 +1,5 @@
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
+let myCredits = localStorage.getItem("Harsh")
+console.log(myCredits)
